d3364793d09288374dc644f3c14119f91ed3a0ca
Fahima Bushra <96842211+fahimabushra1@users.noreply.github.com>
2026-07-07 17:37:30 +0600

    jwks-rsa overrided

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
+  serverExternalPackages: [
+    "firebase-admin",
+  ],
   images: {
     remotePatterns: [
       {
